b3d1e4d330bbbcd493aa3b38caf906547afe3a30
bhawana <bhawanaphartyal08@gmail.com>
2025-08-07 21:48:07 +0530

    07 august program(by recursive)

--- binarysearch.py
+++ binarysearch.py
@@ -1,25 +1,24 @@
 # BINARY SEARCH
 
-
-#Function to find the index of a number
-def binary_search(arr,target):
-    beg = 0
-    end = len(arr)-1
-    while beg <= end:
-        mid = (beg + end)//2
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            beg = mid + 1
-        else:
-            end = mid - 1
+#Function to find the index of a number by recursive funtion
+def binary_search(arr,target,low,high):
+    if low>high:
+        return -1
+    mid = (low + high)//2
+    if arr[mid] == target:
+        return mid
+    elif arr[mid] > target:
+         return binary_search(arr,target,low,mid-1)
+    else:
+        return binary_search(arr,target,mid+1,high)
     return -1
 #main
 lst = [12,67,489,78,78,362,973,748,8,9]
 arr = sorted(lst)
 target = int(input("Enter the number you want to find:"))
-result = binary_search(arr,target)
+result = binary_search(arr,target,0,len(arr)-1)
 if result != -1:
     print("Element",target,"found at index",result)
 else:
     print("Element",target,"not found in the list")
+
