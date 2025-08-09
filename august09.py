87c537752091e9d99dcd500ebb7315f8f356dff9
bhawana <bhawanaphartyal08@gmail.com>
2025-08-09 21:57:41 +0530

    Bubble sort(Aug 9,2025 program)

---
+++ august09.py
@@ -0,0 +1,20 @@
+#BUBBLE SORT
+
+#function for sorting
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        swapped = False
+        for j in range(0,n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j],arr[j+1] = arr[j+1],arr[j]
+                swapped = True
+        if not swapped:
+            break
+    return arr
+
+#main
+my_list = [64,34,25,12,22,11,90]
+sorted_list = bubble_sort(my_list)
+print("Sorted array is :")
+print(sorted_list)
