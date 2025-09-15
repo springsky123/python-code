0e985eb0877b917c6a3e1fd6b621bf9fd6715a82
bhawana <bhawanaphartyal08@gmail.com>
2025-09-15 21:51:44 +0530

    Rainbow Circle Pattern

---
+++ Turtle_programs/rainbowcircle.py
@@ -0,0 +1,13 @@
+import turtle , colorsys
+
+t = turtle.Turtle()
+turtle.bgcolor("black")
+t.speed(0)
+colors = [colorsys.hsv_to_rgb(i/36,1,1) for i in range(36)]
+
+for i in range(36):
+        t.color(colors[i])
+        t.circle(100)
+        t.left(10)
+
+turtle.done()
